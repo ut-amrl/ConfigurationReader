e13f39e0abff55b13ae5362fede629789fb52929
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-08-30 00:39:44 -0400

    Removed unneeded build_lua.sh script.

--- build_lua.sh
+++
@@ -1,8 +0,0 @@
-# Run this script from the home directory to install lua in the proper directory for the ConfigurationReader program
-curl -R -O http://www.lua.org/ftp/lua-5.3.4.tar.gz
-tar zxf lua-5.3.4.tar.gz
-cd lua-5.3.4
-sudo apt-get install libreadline-dev
-make linux test
-sudo make linux install
-rm lua-5.3.4.tar.gz
